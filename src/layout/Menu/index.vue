8161d725a2d90f43d938365c07e0b1ed0d90e4da
Yiming_Lee <Yiming_Lee2001@bupt.edu.cn>
2022-10-25 12:04:45 +0800

    feat(custom): 路由做了一半,视频19的10:18

---
+++ src/layout/Menu/index.vue
@@ -0,0 +1,34 @@
+<template lang="">
+  <el-menu
+    active-text-color="$menuActiveText"
+    background-color="$menuBg"
+    class="el-menu-vertical-demo"
+    default-active="2"
+    text-color="#fff"
+    router
+  >
+    <el-sub-menu :index="item.id" v-for="item in menusList" :key="item.id">
+      <template #title>
+        <span>{{ item.authName }}</span>
+      </template>
+      <el-menu-item
+        :index="'/' + it.path"
+        v-for="it in item.children"
+        :key="it.id"
+        >{{ it.authName }}</el-menu-item
+      >
+    </el-sub-menu>
+  </el-menu>
+</template>
+<script setup>
+import { menuList } from '@/api/menu'
+import { ref } from 'vue'
+
+const menusList = ref([])
+const initMenusList = async () => {
+  menusList.value = await menuList()
+  //   console.log(res)
+}
+initMenusList()
+</script>
+<style lang=""></style>
